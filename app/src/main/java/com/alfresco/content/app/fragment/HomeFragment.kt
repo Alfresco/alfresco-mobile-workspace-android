c29311e6f22463c56a62aab770c53987c18969bd
Alexandru Gologan <alexandru.gologan@gmail.com>
2020-05-19 14:54:27 +0300

    Add home placeholder fab

--- app/src/main/java/com/alfresco/content/app/fragment/HomeFragment.kt
+++ app/src/main/java/com/alfresco/content/app/fragment/HomeFragment.kt
@@ -1,6 +1,20 @@
 package com.alfresco.content.app.fragment
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.alfresco.content.app.R
+
 
 class HomeFragment: Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_home, container, false)
+    }
 }

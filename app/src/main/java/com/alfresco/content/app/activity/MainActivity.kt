05f73a5d05a838c2e31ba5e36940295b9c67c22e
Alexandru Gologan <alexandru.gologan@gmail.com>
2020-05-19 12:24:06 +0300

    Update bottom navigation and screen titles

--- app/src/main/java/com/alfresco/content/app/activity/MainActivity.kt
+++ app/src/main/java/com/alfresco/content/app/activity/MainActivity.kt
@@ -3,11 +3,16 @@ package com.alfresco.content.app.activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.alfresco.content.app.R
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+
+    private val navController by lazy { findNavController(R.id.nav_host_fragment) }
+    private val bottomNav by lazy { findViewById<BottomNavigationView>(R.id.bottom_nav) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -15,8 +20,10 @@ class MainActivity : AppCompatActivity() {
 
         setSupportActionBar(findViewById(R.id.toolbar))
 
-        val navController = findNavController(R.id.nav_host_fragment)
-        findViewById<BottomNavigationView>(R.id.bottom_nav)
-            .setupWithNavController(navController)
+        bottomNav.setupWithNavController(navController)
+        val appBarConfiguration = AppBarConfiguration(bottomNav.menu)
+        setupActionBarWithNavController(navController, appBarConfiguration)
     }
+
+    override fun onSupportNavigateUp(): Boolean = navController.navigateUp()
 }

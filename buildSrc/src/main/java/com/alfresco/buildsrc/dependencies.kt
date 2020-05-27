804a4a1837bb80a60ffb48c660586d7d4deb3957
Alexandru Gologan <alexandru.gologan@gmail.com>
2020-05-27 15:43:13 +0300

    Refactor dependencies using buildSrc plugin
    Refs MOBILEAPPS-328

---
+++ buildSrc/src/main/java/com/alfresco/buildsrc/dependencies.kt
@@ -0,0 +1,88 @@
+package com.alfresco.buildsrc
+
+object Libs {
+
+    const val junit = "junit:junit:4.13"
+
+    object AlfrescoDbp {
+        const val auth = "com.alfresco.dbp.android:auth:0.4-SNAPSHOT"
+        const val core = "com.alfresco.dbp.android:core:0.1-SNAPSHOT"
+        const val content = "com.alfresco.dbp.android:content:0.1-SNAPSHOT"
+    }
+
+    object Kotlin {
+        private const val version = "1.3.72"
+        const val stdlib = "org.jetbrains.kotlin:kotlin-stdlib-jdk8:$version"
+        const val reflect = "org.jetbrains.kotlin:kotlin-reflect:$version"
+        const val gradlePlugin = "org.jetbrains.kotlin:kotlin-gradle-plugin:$version"
+        const val extensions = "org.jetbrains.kotlin:kotlin-android-extensions:$version"
+    }
+
+    object Coroutines {
+        private const val version = "1.3.7"
+        const val core = "org.jetbrains.kotlinx:kotlinx-coroutines-core:$version"
+        const val android = "org.jetbrains.kotlinx:kotlinx-coroutines-android:$version"
+        const val test = "org.jetbrains.kotlinx:kotlinx-coroutines-test:$version"
+    }
+
+    object AndroidX {
+        const val appcompat = "androidx.appcompat:appcompat:1.1.0"
+        const val coreKtx = "androidx.core:core-ktx:1.2.0"
+
+        object Activity {
+            private const val version = "1.1.0"
+            const val activity = "androidx.activity:activity:$version"
+            const val activityKtx = "androidx.activity:activity-ktx:$version"
+        }
+
+        object Fragment {
+            private const val version = "1.2.4"
+            const val fragment = "androidx.fragment:fragment:$version"
+            const val fragmentKtx = "androidx.fragment:fragment-ktx:$version"
+        }
+
+        object Lifecycle {
+            private const val version = "2.2.0"
+            const val extensions = "androidx.lifecycle:lifecycle-extensions:$version"
+            const val viewmodelKtx = "androidx.lifecycle:lifecycle-viewmodel-ktx:$version"
+            const val runtime = "androidx.lifecycle:lifecycle-runtime:$version"
+        }
+
+        const val constraintlayout = "androidx.constraintlayout:constraintlayout:1.1.3"
+
+        const val preference = "androidx.preference:preference:1.1.1"
+
+        object Navigation {
+            private const val version = "2.2.1"
+            const val fragment = "androidx.navigation:navigation-fragment-ktx:$version"
+            const val ui = "androidx.navigation:navigation-ui-ktx:$version"
+        }
+
+        object Test {
+            private const val version = "1.2.0"
+            const val core = "androidx.test:core:$version"
+            const val runner = "androidx.test:runner:$version"
+            const val rules = "androidx.test:rules:$version"
+
+            const val espressoCore = "androidx.test.espresso:espresso-core:3.2.0"
+        }
+    }
+
+    object Google {
+        const val material = "com.google.android.material:material:1.2.0-alpha06"
+    }
+
+    object OkHttp {
+        private const val version = "4.7.2"
+        const val okhttp = "com.squareup.okhttp3:okhttp:$version"
+        const val loggingInterceptor = "com.squareup.okhttp3:logging-interceptor:$version"
+    }
+
+    const val mvRx = "com.airbnb.android:mvrx:1.5.0"
+
+    object Epoxy {
+        private const val version = "3.11.0"
+        const val epoxy = "com.airbnb.android:epoxy:$version"
+        const val processor = "com.airbnb.android:epoxy-processor:$version"
+    }
+}
---
+++ data/src/main/kotlin/com/alfresco/content/data/BrowseRepository.kt
@@ -0,0 +1,42 @@
+package com.alfresco.content.data
+
+import android.content.Context
+import com.alfresco.auth.AuthInterceptor
+import com.alfresco.content.account.Account
+import com.alfresco.content.apis.NodesApi
+import com.alfresco.content.models.NodeChildAssociationEntry
+import com.alfresco.content.tools.GeneratedCodeConverters
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+
+class BrowseRepository(context: Context) {
+
+    private val service: NodesApi by lazy {
+        val acc = Account.getAccount(context)!!
+        val authInterceptor = AuthInterceptor(context, acc.username, acc.authType, acc.authState, acc.authConfig)
+        val loggingInterceptor = HttpLoggingInterceptor()
+        loggingInterceptor.level = HttpLoggingInterceptor.Level.BODY
+
+        val okHttpClient: OkHttpClient = OkHttpClient()
+            .newBuilder()
+            .addInterceptor(authInterceptor)
+            .addInterceptor(loggingInterceptor)
+            .build()
+
+        val retrofit = Retrofit.Builder()
+            .client(okHttpClient)
+            .addConverterFactory(GeneratedCodeConverters.converterFactory())
+            .baseUrl("${acc.serverUrl}/api/-default-/public/")
+            .build()
+        retrofit.create(NodesApi::class.java)
+    }
+
+    suspend fun getNodes(): Flow<List<NodeChildAssociationEntry>> {
+        return flow {
+            emit(service.listNodeChildren("-my-", null, 25, null, null, null, null, null, null).list!!.entries!!)
+        }
+    }
+}

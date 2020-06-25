ead31005bf5927520fc1a6ad6d1f61ecc8c09f6a
Alexandru Gologan <alexandru.gologan@gmail.com>
2020-06-25 15:32:21 +0300

    Change default theme to system value to match settings value

--- app/src/main/java/com/alfresco/content/app/activity/MainActivity.kt
+++ app/src/main/java/com/alfresco/content/app/activity/MainActivity.kt
@@ -132,7 +132,7 @@ class MainActivity : AppCompatActivity(), SharedPreferences.OnSharedPreferenceCh
     }
 
     private fun updateAppTheme() {
-        when (PreferenceManager.getDefaultSharedPreferences(this).getString(getString(R.string.settings_theme_key), getString(R.string.settings_theme_light))) {
+        when (PreferenceManager.getDefaultSharedPreferences(this).getString(getString(R.string.settings_theme_key), getString(R.string.settings_theme_system))) {
             getString(R.string.settings_theme_light) -> AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
             getString(R.string.settings_theme_dark) -> AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
             getString(R.string.settings_theme_system) -> AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM)

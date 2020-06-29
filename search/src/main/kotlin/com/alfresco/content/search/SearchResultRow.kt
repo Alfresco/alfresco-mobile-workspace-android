d9807c9656144fc62299e514ce2dcd78b6957b79
Alexandru Gologan <alexandru.gologan@gmail.com>
2020-06-29 12:02:16 +0300

    Update search results layout to match browsing experience

--- search/src/main/kotlin/com/alfresco/content/search/SearchResultRow.kt
+++ search/src/main/kotlin/com/alfresco/content/search/SearchResultRow.kt
@@ -12,11 +12,8 @@ import com.airbnb.epoxy.ModelView
 import com.alfresco.content.mimetype.MimeType
 import com.alfresco.content.models.ResultNode
 import kotlinx.android.synthetic.main.view_search_result_row.view.icon
-import kotlinx.android.synthetic.main.view_search_result_row.view.path
-import kotlinx.android.synthetic.main.view_search_result_row.view.properties
 import kotlinx.android.synthetic.main.view_search_result_row.view.title
-import org.threeten.bp.format.DateTimeFormatter
-import org.threeten.bp.format.FormatStyle
+import kotlinx.android.synthetic.main.view_search_result_row.view.subtitle
 
 @ModelView(autoLayout = ModelView.Size.MATCH_WIDTH_WRAP_HEIGHT)
 class SearchResultRow @JvmOverloads constructor(
@@ -33,8 +30,7 @@ class SearchResultRow @JvmOverloads constructor(
     fun setNode(node: ResultNode) {
         title.text = node.name
         icon.setImageDrawable(icon(node))
-        properties.text = properties(node)
-        path.text = path(node)
+        subtitle.text = path(node)
     }
 
     @CallbackProp
@@ -47,15 +43,9 @@ class SearchResultRow @JvmOverloads constructor(
         return resources.getDrawable(type.icon, null)
     }
 
-    private fun properties(node: ResultNode): String {
-        val modifiedAt = node.modifiedAt.format(DateTimeFormatter.ofLocalizedDate(FormatStyle.MEDIUM))
-        val modifiedBy = node.modifiedByUser.displayName
-        return "Modified: $modifiedAt by $modifiedBy"
-    }
-
     private fun path(node: ResultNode): String? {
         return node.path?.elements
             ?.map { element -> element.name }
-            ?.reduce { sum, element -> "$sum > $element" }
+            ?.reduce { sum, element -> "$sum \u203A $element" }
     }
 }

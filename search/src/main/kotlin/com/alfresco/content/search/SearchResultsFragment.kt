6e3c7bdc5672a5e0b70594c05c9a006a2078bbdb
Alexandru Gologan <alexandru.gologan@gmail.com>
2020-07-14 19:23:51 +0300

    Dismiss keyboard on search results scroll

--- search/src/main/kotlin/com/alfresco/content/search/SearchResultsFragment.kt
+++ search/src/main/kotlin/com/alfresco/content/search/SearchResultsFragment.kt
@@ -1,6 +1,7 @@
 package com.alfresco.content.search
 
 import com.airbnb.mvrx.fragmentViewModel
+import com.alfresco.content.HideSoftInputOnScrollListener
 import com.alfresco.content.data.Entry
 import com.alfresco.content.data.SearchFilters
 import com.alfresco.content.listview.ListFragment
@@ -8,6 +9,12 @@ import com.alfresco.content.listview.ListFragment
 class SearchResultsFragment : ListFragment<SearchResultsViewModel>() {
 
     override val viewModel: SearchResultsViewModel by fragmentViewModel()
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        recyclerView.addOnScrollListener(HideSoftInputOnScrollListener())
+    }
 
     fun setSearchQuery(query: String) {
         viewModel.setSearchQuery(query)

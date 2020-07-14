c56ba119d94d2aa46d89593b247e67e2ee44ee8b
Alexandru Gologan <alexandru.gologan@gmail.com>
2020-07-14 19:19:56 +0300

    Add helpers for managing soft input

---
+++ common/src/main/kotlin/com/alfresco/content/ContextExt.kt
@@ -0,0 +1,32 @@
+package com.alfresco.content
+
+import android.app.Activity
+import android.content.Context
+import android.content.ContextWrapper
+import android.view.inputmethod.InputMethodManager
+import androidx.core.content.getSystemService
+import androidx.fragment.app.Fragment
+
+inline fun <reified T : Context> Context.findBaseContext(): T? {
+    var ctx: Context? = this
+    do {
+        if (ctx is T) {
+            return ctx
+        }
+        if (ctx is ContextWrapper) {
+            ctx = ctx.baseContext
+        }
+    } while (ctx != null)
+
+    return null
+}
+
+fun Activity.hideSoftInput() {
+    val imm: InputMethodManager? = getSystemService()
+    val currentFocus = currentFocus
+    if (currentFocus != null && imm != null) {
+        imm.hideSoftInputFromWindow(currentFocus.windowToken, 0)
+    }
+}
+
+fun Fragment.hideSoftInput() = requireActivity().hideSoftInput()
---
+++ common/src/main/kotlin/com/alfresco/content/HideSoftInputOnScrollListener.kt
@@ -0,0 +1,25 @@
+package com.alfresco.content
+
+import android.app.Activity
+import android.view.inputmethod.InputMethodManager
+import androidx.core.content.getSystemService
+import androidx.recyclerview.widget.RecyclerView
+
+class HideSoftInputOnScrollListener : RecyclerView.OnScrollListener() {
+    private var imm: InputMethodManager? = null
+
+    override fun onScrollStateChanged(recyclerView: RecyclerView, newState: Int) {
+        if (newState == RecyclerView.SCROLL_STATE_DRAGGING && recyclerView.childCount > 0) {
+            if (imm == null) {
+                imm = recyclerView.context.getSystemService()
+            }
+            if (imm?.isAcceptingText == true) {
+                val activity: Activity? = recyclerView.context.findBaseContext()
+                val currentFocus = activity?.currentFocus
+                if (currentFocus != null) {
+                    imm?.hideSoftInputFromWindow(currentFocus.windowToken, 0)
+                }
+            }
+        }
+    }
+}

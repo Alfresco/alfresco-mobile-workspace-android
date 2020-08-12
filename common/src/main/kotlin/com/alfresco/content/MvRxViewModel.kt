b55128b76d115d8de9cbffab78cc3f1e71be8e91
Alexandru Gologan <alexandru.gologan@gmail.com>
2020-08-12 14:03:43 +0300

    Remove unused liveData helpers from mvrx view model

--- common/src/main/kotlin/com/alfresco/content/MvRxViewModel.kt
+++ common/src/main/kotlin/com/alfresco/content/MvRxViewModel.kt
@@ -1,36 +1,23 @@
 package com.alfresco.content
 
 import android.util.Log
-import androidx.lifecycle.LifecycleOwner
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import com.airbnb.mvrx.Async
 import com.airbnb.mvrx.BaseMvRxViewModel
-import com.airbnb.mvrx.DeliveryMode
 import com.airbnb.mvrx.Fail
 import com.airbnb.mvrx.Loading
 import com.airbnb.mvrx.MvRxState
 import com.airbnb.mvrx.Success
 import com.alfresco.content.common.BuildConfig
-import io.reactivex.disposables.Disposable
-import java.lang.Exception
-import kotlin.reflect.KProperty1
+import java.util.concurrent.CancellationException
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.flow.map
-import java.util.concurrent.CancellationException
 
 open class MvRxViewModel<S : MvRxState>(
     initialState: S
 ) : BaseMvRxViewModel<S>(initialState, debugMode = BuildConfig.DEBUG) {
-
-    private val liveData by lazy(LazyThreadSafetyMode.NONE) {
-        MvRxStateLiveData<S> {
-            subscribe { value = it }
-        }
-    }
 
     protected suspend fun <T> Flow<T>.execute(
         stateReducer: S.(Async<T>) -> S
@@ -67,35 +54,6 @@ open class MvRxViewModel<S : MvRxState>(
                 // No-op
             } catch (e: Throwable) {
                 setState { stateReducer(Fail(e)) }
-            }
-        }
-    }
-
-    fun observeAsLiveData(): LiveData<S> = liveData
-
-    fun <A> selectObserve(
-        owner: LifecycleOwner,
-        prop1: KProperty1<S, A>,
-        deliveryMode: DeliveryMode
-    ): LiveData<A> = MvRxStateLiveData {
-        selectSubscribe(owner, prop1, deliveryMode) { value = it }
-    }
-
-    private class MvRxStateLiveData<T>(
-        private val subscribe: MvRxStateLiveData<T>.() -> Disposable
-    ) : MutableLiveData<T>() {
-        private var disposable: Disposable? = null
-
-        override fun onActive() {
-            disposable = subscribe()
-        }
-
-        override fun onInactive() {
-            val d = disposable
-            disposable = null
-
-            if (d != null && !d.isDisposed) {
-                d.dispose()
             }
         }
     }

9272d80ea0ee8e36f92273549c3bd53a9537873c
Alexandru Gologan <alexandru.gologan@gmail.com>
2020-09-03 18:46:06 +0300

    Pre-download images with download manager to improve performance

--- viewer/src/main/kotlin/com/alfresco/content/viewer/ImageViewerFragment.kt
+++ viewer/src/main/kotlin/com/alfresco/content/viewer/ImageViewerFragment.kt
@@ -1,29 +1,77 @@
 package com.alfresco.content.viewer
 
+import android.app.DownloadManager
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
 import android.os.Bundle
 import android.view.View
+import android.widget.ProgressBar
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.lifecycleScope
 import com.alfresco.content.data.BrowseRepository
 import com.davemorrissey.labs.subscaleview.ImageSource
 import com.davemorrissey.labs.subscaleview.SubsamplingScaleImageView
-import kotlinx.coroutines.launch
+import java.io.File
 
 class ImageViewerFragment(
     private val documentId: String,
     private val mimeType: String
 ) : Fragment(R.layout.viewer_image) {
 
+    private lateinit var downloadMonitor: DownloadStateReceiver
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        // TODO: Stream data and improve performance
+        val progressIndicator = view.findViewById<ProgressBar>(R.id.progress_indicator)
         val imageView = view.findViewById<SubsamplingScaleImageView>(R.id.imageView)
-        lifecycleScope.launch {
-            val stream = BrowseRepository().fetchContentStream(documentId)
-            imageView.setBitmapDecoderFactory(InputStreamImageDecoder.Factory(stream))
-            imageView.setRegionDecoderFactory(InputStreamImageRegionDecoder.Factory(stream))
-            imageView.setImage(ImageSource.asset("dummy"))
+
+        val path = requireContext().getExternalFilesDir(null)?.path + "/" + documentId
+        val renderImage = {
+            progressIndicator.visibility = View.GONE
+            imageView.setImage(ImageSource.uri(path))
+        }
+
+        val download = File(path)
+        if (download.exists()) {
+            renderImage()
+            return
+        }
+
+        val dm = ContextCompat.getSystemService(requireContext(), DownloadManager::class.java)
+        val request = DownloadManager.Request(BrowseRepository().contentUri(documentId))
+        request.setAllowedNetworkTypes(DownloadManager.Request.NETWORK_WIFI or DownloadManager.Request.NETWORK_MOBILE)
+        request.setTitle(documentId)
+        request.setDescription("")
+        request.setDestinationInExternalFilesDir(requireContext(), null, documentId)
+
+        val ref = dm?.enqueue(request)
+
+        downloadMonitor = DownloadStateReceiver(ref ?: -1, renderImage)
+
+        val filter = IntentFilter(DownloadManager.ACTION_DOWNLOAD_COMPLETE)
+        requireContext().registerReceiver(downloadMonitor, filter)
+    }
+
+    class DownloadStateReceiver(
+        private val downloadId: Long,
+        private val onComplete: () -> Unit
+    ): BroadcastReceiver() {
+        override fun onReceive(context: Context?, intent: Intent?) {
+            val referenceId = intent?.getLongExtra(DownloadManager.EXTRA_DOWNLOAD_ID, -1)
+            if (referenceId == downloadId) {
+                onComplete()
+            }
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+
+        if (this::downloadMonitor.isInitialized) {
+            requireContext().unregisterReceiver(downloadMonitor)
         }
     }
 }

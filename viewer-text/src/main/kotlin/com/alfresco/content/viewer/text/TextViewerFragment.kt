8e1475643b5e1d2591ec7d2317dcf5830fcba199
Alexandru Gologan <alexandru.gologan@gmail.com>
2020-09-18 12:10:03 +0300

    Replace text viewer component with webView

--- viewer-text/src/main/kotlin/com/alfresco/content/viewer/text/TextViewerFragment.kt
+++ viewer-text/src/main/kotlin/com/alfresco/content/viewer/text/TextViewerFragment.kt
@@ -1,9 +1,10 @@
 package com.alfresco.content.viewer.text
 
 import android.os.Bundle
-import android.text.method.ScrollingMovementMethod
 import android.view.View
-import android.widget.TextView
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import android.widget.ProgressBar
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.lifecycleScope
 import com.alfresco.content.viewer.common.ContentDownloader
@@ -17,18 +18,31 @@ class TextViewerFragment(
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        val textView = view.findViewById<TextView>(R.id.textView)
-        textView.movementMethod = ScrollingMovementMethod()
+        val webView = view.findViewById<WebView>(R.id.webview)
+        val progressIndicator = view.findViewById<ProgressBar>(R.id.progress_indicator)
+
+        val settings = webView.settings
+        settings.setSupportZoom(true)
+        settings.builtInZoomControls = true
+        settings.displayZoomControls = false
+        settings.savePassword = false
+        settings.saveFormData = false
+        settings.blockNetworkLoads = true
+        settings.allowFileAccess = true
+        settings.javaScriptEnabled = false
+        settings.defaultTextEncodingName = "utf-8"
+
+        webView.webViewClient = object : WebViewClient() {
+            override fun onPageFinished(view: WebView?, url: String?) {
+                progressIndicator.visibility = View.GONE
+            }
+        }
+
         val output = File(requireContext().cacheDir, "content.tmp")
 
         lifecycleScope.launch {
             ContentDownloader.downloadFileTo(uri, output.path)
-
-            // progressIndicator.visibility = View.GONE
-            textView.text = readFile(output.path)
+            webView.loadUrl("file://${output.path}")
         }
     }
-
-    private fun readFile(fileName: String) =
-        File(fileName).inputStream().readBytes().toString(Charsets.UTF_8)
 }

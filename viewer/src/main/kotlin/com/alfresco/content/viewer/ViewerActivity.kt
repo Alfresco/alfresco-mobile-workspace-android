09afa338a91aa0503175c9985d9124c816d264da
Alexandru Gologan <alexandru.gologan@gmail.com>
2020-09-24 19:41:55 +0300

    Convert viewer fragment to activity to enable independent customization

---
+++ viewer/src/main/kotlin/com/alfresco/content/viewer/ViewerActivity.kt
@@ -0,0 +1,29 @@
+package com.alfresco.content.viewer
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.fragment.app.Fragment
+
+class ViewerActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_viewer)
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
+
+        supportActionBar?.setDisplayShowHomeEnabled(true)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        toolbar.setNavigationOnClickListener { onBackPressed() }
+
+        title = intent.extras?.getString("title")
+    }
+
+    override fun onAttachFragment(fragment: Fragment) {
+        super.onAttachFragment(fragment)
+
+        fragment.arguments = intent.extras
+    }
+}

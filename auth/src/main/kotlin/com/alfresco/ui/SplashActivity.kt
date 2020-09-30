372428b1cf97114f183db55d91894758ebda0df9
Alexandru Gologan <alexandru.gologan@gmail.com>
2020-09-30 20:07:58 +0300

    Update application branding (icon, splash, login)

--- auth/src/main/kotlin/com/alfresco/ui/SplashActivity.kt
+++ auth/src/main/kotlin/com/alfresco/ui/SplashActivity.kt
@@ -3,22 +3,17 @@ package com.alfresco.ui
 import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
-import android.view.animation.DecelerateInterpolator
-import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 import com.alfresco.android.aims.R
 
 abstract class SplashActivity : AppCompatActivity() {
 
     private val handler = Handler()
-    private lateinit var logoImageView: ImageView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContentView(R.layout.activity_alfresco_splash)
-
-        logoImageView = findViewById(R.id.ivLogo)
     }
 
     override fun onPause() {
@@ -30,18 +25,6 @@ abstract class SplashActivity : AppCompatActivity() {
 
     override fun onResume() {
         super.onResume()
-
-        logoImageView.scaleX = 0.8f
-        logoImageView.scaleY = 0.8f
-
-        logoImageView.apply {
-            animate()
-                    .scaleX(1f)
-                    .scaleY(1f)
-                    .setDuration(ANIMATION_DURATION)
-                    .setInterpolator(DecelerateInterpolator())
-                    .setListener(null)
-        }
 
         // Transition after delay
         handler.postDelayed({
@@ -59,7 +42,6 @@ abstract class SplashActivity : AppCompatActivity() {
     abstract fun getMainIntent(): Intent
 
     companion object {
-        private const val ANIMATION_DURATION = 2000L
-        private const val DISPLAY_TIMEOUT = 2200L
+        private const val DISPLAY_TIMEOUT = 100L
     }
 }

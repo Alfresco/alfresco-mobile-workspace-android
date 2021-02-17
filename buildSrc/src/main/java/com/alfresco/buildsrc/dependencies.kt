deaf98674cde6df64f2a50adaaf0e37bc6ea7a18
Alexandru Gologan <alexandru.gologan@gmail.com>
2021-02-17 10:05:07 +0200

    Update all dependencies to their stable releases

--- buildSrc/src/main/java/com/alfresco/buildsrc/dependencies.kt
+++ buildSrc/src/main/java/com/alfresco/buildsrc/dependencies.kt
@@ -4,19 +4,19 @@ package com.alfresco.buildsrc
 object Libs {
 
     object AndroidTools {
-        const val gradlePlugin = "com.android.tools.build:gradle:4.1.1"
+        const val gradlePlugin = "com.android.tools.build:gradle:4.1.2"
         const val desugar = "com.android.tools:desugar_jdk_libs:1.0.10"
     }
 
-    const val junit = "junit:junit:4.13.1"
+    const val junit = "junit:junit:4.13.2"
 
-    const val spotless = "com.diffplug.spotless:spotless-plugin-gradle:5.8.2"
+    const val spotless = "com.diffplug.spotless:spotless-plugin-gradle:5.10.2"
 
     const val gradleVersions = "com.github.ben-manes:gradle-versions-plugin:0.36.0"
 
     const val leakCanary = "com.squareup.leakcanary:leakcanary-android:2.5"
 
-    const val objectBox = "io.objectbox:objectbox-gradle-plugin:2.8.1"
+    const val objectBox = "io.objectbox:objectbox-gradle-plugin:2.9.0"
 
     object Alfresco {
         const val auth = "com.alfresco.android:auth:0.7-SNAPSHOT"
@@ -25,7 +25,7 @@ object Libs {
     }
 
     object Kotlin {
-        private const val version = "1.4.20"
+        private const val version = "1.4.30"
         const val stdlib = "org.jetbrains.kotlin:kotlin-stdlib-jdk8:$version"
         const val reflect = "org.jetbrains.kotlin:kotlin-reflect:$version"
         const val gradlePlugin = "org.jetbrains.kotlin:kotlin-gradle-plugin:$version"
@@ -43,23 +43,23 @@ object Libs {
         const val coreKtx = "androidx.core:core-ktx:1.3.2"
 
         object Activity {
-            private const val version = "1.1.0"
+            private const val version = "1.2.0"
             const val activity = "androidx.activity:activity:$version"
             const val activityKtx = "androidx.activity:activity-ktx:$version"
         }
 
         object Fragment {
-            private const val version = "1.2.5"
+            private const val version = "1.3.0"
             const val fragment = "androidx.fragment:fragment:$version"
             const val fragmentKtx = "androidx.fragment:fragment-ktx:$version"
         }
 
         object Lifecycle {
-            private const val version = "2.2.0"
-            const val viewmodelKtx = "androidx.lifecycle:lifecycle-viewmodel-ktx:$version"
+            private const val version = "2.3.0"
+            const val viewModelKtx = "androidx.lifecycle:lifecycle-viewmodel-ktx:$version"
+            const val liveDataKtx = "androidx.lifecycle:lifecycle-livedata-ktx:$version"
             const val runtime = "androidx.lifecycle:lifecycle-runtime-ktx:$version"
             const val process = "androidx.lifecycle:lifecycle-process:$version"
-            const val viewModelKtx = "androidx.lifecycle:lifecycle-livedata-ktx:$version"
         }
 
         const val recyclerview = "androidx.recyclerview:recyclerview:1.1.0"
@@ -74,10 +74,10 @@ object Libs {
 
         const val webkit = "androidx.webkit:webkit:1.4.0"
 
-        const val work = "androidx.work:work-runtime-ktx:2.4.0"
+        const val work = "androidx.work:work-runtime-ktx:2.5.0"
 
         object Navigation {
-            private const val version = "2.3.1"
+            private const val version = "2.3.3"
             const val fragment = "androidx.navigation:navigation-fragment-ktx:$version"
             const val ui = "androidx.navigation:navigation-ui-ktx:$version"
         }
@@ -93,19 +93,19 @@ object Libs {
     }
 
     object Google {
-        const val material = "com.google.android.material:material:1.3.0-alpha04"
+        const val material = "com.google.android.material:material:1.3.0"
 
-        const val servicesPlugin = "com.google.gms:google-services:4.3.4"
+        const val servicesPlugin = "com.google.gms:google-services:4.3.5"
 
         object Firebase {
-            const val analytics = "com.google.firebase:firebase-analytics-ktx:18.0.0"
-            const val crashlytics = "com.google.firebase:firebase-crashlytics-ktx:17.3.0"
-            const val crashlyticsPlugin = "com.google.firebase:firebase-crashlytics-gradle:2.4.1"
+            const val analytics = "com.google.firebase:firebase-analytics-ktx:18.0.2"
+            const val crashlytics = "com.google.firebase:firebase-crashlytics-ktx:17.3.1"
+            const val crashlyticsPlugin = "com.google.firebase:firebase-crashlytics-gradle:2.5.0"
         }
     }
 
     object OkHttp {
-        private const val version = "4.9.0"
+        private const val version = "4.9.1"
         const val okhttp = "com.squareup.okhttp3:okhttp:$version"
         const val loggingInterceptor = "com.squareup.okhttp3:logging-interceptor:$version"
     }
@@ -113,20 +113,20 @@ object Libs {
     const val mvRx = "com.airbnb.android:mvrx:1.5.1"
 
     object Epoxy {
-        private const val version = "4.1.0"
+        private const val version = "4.3.1"
         const val epoxy = "com.airbnb.android:epoxy:$version"
         const val processor = "com.airbnb.android:epoxy-processor:$version"
     }
 
     object Coil {
-        private const val version = "1.1.0"
+        private const val version = "1.1.1"
         const val core = "io.coil-kt:coil:$version"
         const val gifExt = "io.coil-kt:coil-gif:$version"
         const val svgExt = "io.coil-kt:coil-svg:$version"
     }
 
     object ExoPlayer {
-        private const val version = "2.12.1"
+        private const val version = "2.13.1"
         const val core = "com.google.android.exoplayer:exoplayer-core:$version"
         const val ui = "com.google.android.exoplayer:exoplayer-ui:$version"
     }

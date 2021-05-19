18d2efa32216c65f200340afb262123732ff9649
Alexandru Gologan <alexandru.gologan@gmail.com>
2021-05-19 17:59:33 +0300

    Use trashcan api extension to restore deleted node

--- data/src/main/kotlin/com/alfresco/content/data/TrashCanRepository.kt
+++ data/src/main/kotlin/com/alfresco/content/data/TrashCanRepository.kt
@@ -2,12 +2,17 @@ package com.alfresco.content.data
 
 import com.alfresco.content.apis.AlfrescoApi
 import com.alfresco.content.apis.TrashcanApi
+import com.alfresco.content.apis.TrashcanApiExt
 import com.alfresco.content.session.Session
 import com.alfresco.content.session.SessionManager
 
 class TrashCanRepository(val session: Session = SessionManager.requireSession) {
     private val service: TrashcanApi by lazy {
         session.createService(TrashcanApi::class.java)
+    }
+
+    private val serviceExt: TrashcanApiExt by lazy {
+        session.createService(TrashcanApiExt::class.java)
     }
 
     suspend fun getDeletedNodes(skipCount: Int, maxItems: Int) =
@@ -18,7 +23,7 @@ class TrashCanRepository(val session: Session = SessionManager.requireSession) {
         ))
 
     suspend fun restoreEntry(entry: Entry) =
-        Entry.with(service.restoreDeletedNode(entry.id).entry)
+        Entry.with(serviceExt.restoreDeletedNode(entry.id).entry)
 
     suspend fun deleteForeverEntry(entry: Entry) =
         service.deleteDeletedNode(entry.id)

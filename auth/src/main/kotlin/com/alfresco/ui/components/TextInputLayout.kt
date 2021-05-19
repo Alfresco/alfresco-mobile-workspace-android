a5ec35d4e9e04cefe5ac0722e2fd4cd360a59708
Alexandru Gologan <alexandru.gologan@gmail.com>
2021-05-19 17:46:15 +0300

    Fix TextInputLayout workaround implementation checking added view type

--- auth/src/main/kotlin/com/alfresco/ui/components/TextInputLayout.kt
+++ auth/src/main/kotlin/com/alfresco/ui/components/TextInputLayout.kt
@@ -6,6 +6,7 @@ import android.text.TextWatcher
 import android.util.AttributeSet
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
 import com.google.android.material.R
 import com.google.android.material.textfield.TextInputLayout
 
@@ -19,25 +20,31 @@ class TextInputLayout(context: Context, attrs: AttributeSet?, defStyleAttr: Int)
         this(context, attrs, R.attr.textInputStyle)
 
     override fun addView(child: View, index: Int, params: ViewGroup.LayoutParams) {
-        super.addView(child, index, params)
+        if (child is EditText) {
+            child.addTextChangedListener(object : TextWatcher {
+                override fun afterTextChanged(s: Editable?) {
+                    val editText = editText ?: return
 
-        editText?.addTextChangedListener(object : TextWatcher {
-            override fun afterTextChanged(s: Editable?) {
-                val editText = editText ?: return
-
-                // Changing text programmatically incorrectly displays clear text icon
-                if (!editText.isFocused && endIconMode == END_ICON_CLEAR_TEXT) {
-                    isEndIconVisible = false
+                    // Changing text programmatically incorrectly displays clear text icon
+                    if (!editText.isFocused && endIconMode == END_ICON_CLEAR_TEXT) {
+                        isEndIconVisible = false
+                    }
                 }
-            }
 
-            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
-                // no-op
-            }
+                override fun beforeTextChanged(
+                    s: CharSequence?,
+                    start: Int,
+                    count: Int,
+                    after: Int
+                ) {
+                    // no-op
+                }
 
-            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-                // no-op
-            }
-        })
+                override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                    // no-op
+                }
+            })
+        }
+        super.addView(child, index, params)
     }
 }

0a0be15e87de7cc6b8cbaff9f219c90ef3f8b624
Alexandru Gologan <alexandru.gologan@gmail.com>
2021-05-21 15:05:02 +0300

    Change filename comparator to ignore case
    Refs: MOBILEAPPS-731

--- base/src/main/kotlin/com/alfresco/kotlin/FilenameComparator.kt
+++ base/src/main/kotlin/com/alfresco/kotlin/FilenameComparator.kt
@@ -2,9 +2,9 @@ package com.alfresco.kotlin
 
 object FilenameComparator : java.util.Comparator<String> {
     override fun compare(s1: String, s2: String): Int {
-        val r = base(s1).compareTo(base(s2))
+        val r = base(s1).compareTo(base(s2), ignoreCase = true)
         return if (r != 0) r
-        else ext(s1).compareTo(ext(s2))
+        else ext(s1).compareTo(ext(s2), ignoreCase = true)
     }
 
     private fun base(s: String) =

925c6237833e12f4b41cc8dee3f2175a59b62662
aman-alfresco <87360222+aman-alfresco@users.noreply.github.com>
2021-07-22 17:23:00 +0530

    code correction

--- capture/src/main/kotlin/com/alfresco/capture/LocationUtils.kt
+++ capture/src/main/kotlin/com/alfresco/capture/LocationUtils.kt
@@ -1,10 +1,7 @@
 package com.alfresco.capture
 
 import android.content.Context
-import android.content.Intent
 import android.location.LocationManager
-import android.provider.Settings
-import androidx.appcompat.app.AlertDialog
 
 object LocationUtils {
 
@@ -16,19 +13,5 @@ object LocationUtils {
             context.getSystemService(Context.LOCATION_SERVICE) as LocationManager
         return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER) ||
                 locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER)
-    }
-
-    /**
-     * Function to show the "enable GPS" Dialog box
-     */
-    fun showGPSNotEnabledDialog(context: Context) {
-        AlertDialog.Builder(context)
-            .setTitle(context.getString(R.string.enable_gps))
-            .setMessage(context.getString(R.string.required_for_this_app))
-            .setCancelable(false)
-            .setPositiveButton(context.getString(R.string.enable_now)) { _, _ ->
-                context.startActivity(Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS))
-            }
-            .show()
     }
 }

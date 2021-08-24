9920f3202324b3ba25570c4c06229932b46ef3c0
Amanpal Singh <87360222+aman-alfresco@users.noreply.github.com>
2021-08-24 09:07:05 +0530

    Create Folder #Mobileapps 626 (#23)
    * add metadata to capture images
    
    * working on create folder
    
    * code correction
    
    * working on create folder
    
    * create folder dialog to handle the input data
    
    * added properties in node create object
    
    * added icon and update string format for folder creation
    
    * resolved conflicts
    
    * removed spacing in code
    
    * refresh screen after creation of folder
    
    * resolved conflicts
    
    * code correction
    
    * done short-end correction
    
    * Improve create folder dialog style/layout to better match material design
    
    * Reformat create dialog xml
    
    * Use an alternative way to show the create folder dialog
    
    * Fix create folder dialog being presented full width
    
    * Remove unused view model for create folder dialog
    
    * Simplify create folder dialog creation using a custom view in alert dialog
    
    * Remove unused plugin/libs in actions module
    
    * Return created folder entry via the action

---
+++ actions/src/main/kotlin/com/alfresco/content/actions/ActionCreateFolder.kt
@@ -0,0 +1,44 @@
+package com.alfresco.content.actions
+
+import android.content.Context
+import android.view.View
+import com.alfresco.content.data.BrowseRepository
+import com.alfresco.content.data.Entry
+import kotlin.coroutines.resume
+import kotlin.coroutines.suspendCoroutine
+import kotlinx.coroutines.CancellationException
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+
+data class ActionCreateFolder(
+    override var entry: Entry,
+    override val icon: Int = R.drawable.ic_action_create_folder,
+    override val title: Int = R.string.action_create_folder
+) : Action {
+
+    override suspend fun execute(context: Context): Entry {
+        val result = showCreateFolderDialog(context) ?: throw CancellationException("User Cancellation")
+        return BrowseRepository().createFolder(result.name, result.description, entry.id)
+    }
+
+    private suspend fun showCreateFolderDialog(context: Context) = withContext(Dispatchers.Main) {
+        suspendCoroutine<CreateFolderMetadata?> {
+            CreateFolderDialog.Builder(context)
+                .onSuccess { title, description ->
+                    it.resume(CreateFolderMetadata(title, description))
+                }
+                .onCancel { it.resume(null) }
+                .show()
+        }
+    }
+
+    override fun copy(_entry: Entry): Action = copy(entry = _entry)
+
+    override fun showToast(view: View, anchorView: View?) =
+        Action.showToast(view, anchorView, R.string.action_create_folder_toast, entry.name)
+
+    private data class CreateFolderMetadata(
+        val name: String,
+        val description: String
+    )
+}

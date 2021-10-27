99f22a11b9ef1818e2fca5da2be9c2c07a4cd545
Amanpal Singh <87360222+aman-alfresco@users.noreply.github.com>
2021-10-27 12:42:11 +0530

    Mobileapps 888 (#40)
    * added layout for check list
    
    * check list component added
    
    * fixed documentation

---
+++ search/src/main/kotlin/com/alfresco/content/search/components/ListViewCheckRow.kt
@@ -0,0 +1,42 @@
+package com.alfresco.content.search.components
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.LayoutInflater
+import android.widget.CompoundButton
+import android.widget.FrameLayout
+import com.airbnb.epoxy.CallbackProp
+import com.airbnb.epoxy.ModelProp
+import com.airbnb.epoxy.ModelView
+import com.alfresco.content.models.Options
+import com.alfresco.content.search.databinding.ViewCheckListRowBinding
+
+@ModelView(autoLayout = ModelView.Size.MATCH_WIDTH_WRAP_HEIGHT)
+internal class ListViewCheckRow @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null,
+    defStyleAttr: Int = 0
+) : FrameLayout(context, attrs, defStyleAttr) {
+    private val binding = ViewCheckListRowBinding.inflate(LayoutInflater.from(context), this, true)
+
+    @ModelProp
+    fun setData(options: Options) {
+        println("ListViewCheckRow.setData")
+        binding.title.text = options.name
+    }
+
+    @ModelProp
+    fun setOptionSelected(isSelected: Boolean) {
+        binding.checkBox.isChecked = isSelected
+    }
+
+    @CallbackProp
+    fun setClickListener(listener: OnClickListener?) {
+        binding.checkBox.setOnClickListener(listener)
+    }
+
+    @CallbackProp
+    fun setCheckedChangeListener(listener: CompoundButton.OnCheckedChangeListener?) {
+        binding.checkBox.setOnCheckedChangeListener(listener)
+    }
+}

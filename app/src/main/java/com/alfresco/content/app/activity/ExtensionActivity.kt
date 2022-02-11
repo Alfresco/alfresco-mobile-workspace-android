62b0f32ed1b60b6c6045171b07723b7d2eb1dece
aman-alfresco <87360222+aman-alfresco@users.noreply.github.com>
2022-02-11 17:56:28 +0530

    checking if session is available or not

--- app/src/main/java/com/alfresco/content/app/activity/ExtensionActivity.kt
+++ app/src/main/java/com/alfresco/content/app/activity/ExtensionActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
 import android.os.Parcelable
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import com.airbnb.mvrx.MavericksView
@@ -11,6 +12,8 @@ import com.airbnb.mvrx.withState
 import com.alfresco.content.activityViewModel
 import com.alfresco.content.app.R
 import com.alfresco.content.app.widget.ActionBarController
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
+import java.lang.ref.WeakReference
 
 /**
  * Marked as ExtensionActivity class
@@ -20,6 +23,7 @@ class ExtensionActivity : AppCompatActivity(), MavericksView {
     private val viewModel: MainActivityViewModel by activityViewModel()
     private val navController by lazy { findNavController(R.id.nav_host_fragment) }
     private lateinit var actionBarController: ActionBarController
+    private var signedOutDialog = WeakReference<AlertDialog>(null)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -64,6 +68,30 @@ class ExtensionActivity : AppCompatActivity(), MavericksView {
         }
     }
 
-    override fun invalidate() = withState(viewModel) {
+    override fun invalidate() = withState(viewModel) { state ->
+
+        if (state.requiresReLogin) {
+            if (state.isOnline) {
+                showSignedOutPrompt()
+            }
+        } else {
+            // Only when logged in otherwise triggers re-login prompts
+            actionBarController.setProfileIcon(viewModel.profileIcon)
+        }
+
+        actionBarController.setOnline(state.isOnline)
+    }
+
+    private fun showSignedOutPrompt() {
+        val oldDialog = signedOutDialog.get()
+        if (oldDialog != null && oldDialog.isShowing) return
+        val dialog = MaterialAlertDialogBuilder(this)
+            .setTitle(resources.getString(R.string.auth_signed_out_title))
+            .setMessage(resources.getString(R.string.auth_signed_out_subtitle))
+            .setNegativeButton(resources.getString(R.string.sign_out_confirmation_negative), null)
+            .setPositiveButton(resources.getString(R.string.auth_basic_sign_in_button)) { _, _ ->
+            }
+            .show()
+        signedOutDialog = WeakReference(dialog)
     }
 }

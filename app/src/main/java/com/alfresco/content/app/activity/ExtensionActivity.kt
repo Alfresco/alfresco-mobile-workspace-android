8c56dd561a10e2991b16fd3287c63d6bd32ff8a2
Amanpal Singh <87360222+aman-alfresco@users.noreply.github.com>
2022-02-17 10:16:26 +0530

    Mobileapps 1076 (#56)
    * checking if session is available or not (#78)
    
    * Mobileapps 1054 (#79)
    
    Login user if session is expired
    
    Co-authored-by: AnniNygren <anni.nygren@alfresco.com>
    Co-authored-by: Gloria Camino <gloria.broadbent@alfresco.com>
    Co-authored-by: Lovekesh Saxena <lovekesh.saxena@contractors.onbase.com>

--- app/src/main/java/com/alfresco/content/app/activity/ExtensionActivity.kt
+++ app/src/main/java/com/alfresco/content/app/activity/ExtensionActivity.kt
@@ -4,13 +4,18 @@ import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
 import android.os.Parcelable
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import com.airbnb.mvrx.MavericksView
 import com.airbnb.mvrx.withState
+import com.alfresco.auth.activity.LoginViewModel
 import com.alfresco.content.activityViewModel
 import com.alfresco.content.app.R
 import com.alfresco.content.app.widget.ActionBarController
+import com.alfresco.content.session.SessionManager
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
+import java.lang.ref.WeakReference
 
 /**
  * Marked as ExtensionActivity class
@@ -20,6 +25,7 @@ class ExtensionActivity : AppCompatActivity(), MavericksView {
     private val viewModel: MainActivityViewModel by activityViewModel()
     private val navController by lazy { findNavController(R.id.nav_host_fragment) }
     private lateinit var actionBarController: ActionBarController
+    private var signedOutDialog = WeakReference<AlertDialog>(null)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -64,6 +70,48 @@ class ExtensionActivity : AppCompatActivity(), MavericksView {
         }
     }
 
-    override fun invalidate() = withState(viewModel) {
+    override fun invalidate() = withState(viewModel) { state ->
+
+        if (state.requiresReLogin) {
+            if (state.isOnline) {
+                showSignedOutPrompt()
+            }
+        } else {
+            // Only when logged in otherwise triggers re-login prompts
+            actionBarController.setProfileIcon(viewModel.profileIcon)
+        }
+
+        actionBarController.setOnline(state.isOnline)
+    }
+
+    private fun showSignedOutPrompt() {
+        val oldDialog = signedOutDialog.get()
+        if (oldDialog != null && oldDialog.isShowing) return
+        val dialog = MaterialAlertDialogBuilder(this)
+            .setTitle(resources.getString(R.string.auth_signed_out_title))
+            .setMessage(resources.getString(R.string.auth_signed_out_subtitle))
+            .setNegativeButton(resources.getString(R.string.sign_out_confirmation_negative), null)
+            .setPositiveButton(resources.getString(R.string.auth_basic_sign_in_button)) { _, _ ->
+                navigateToReLogin()
+            }
+            .show()
+        signedOutDialog = WeakReference(dialog)
+    }
+
+    override fun onNewIntent(intent: Intent?) {
+        super.onNewIntent(intent)
+        println("ExtensionActivity.onNewIntent")
+    }
+
+    private fun navigateToReLogin() {
+        val i = Intent(this, LoginActivity::class.java)
+        val acc = SessionManager.requireSession.account
+        i.putExtra(LoginViewModel.EXTRA_IS_EXTENSION, true)
+        i.putExtra(LoginViewModel.EXTRA_ENDPOINT, acc.serverUrl)
+        i.putExtra(LoginViewModel.EXTRA_AUTH_TYPE, acc.authType)
+        i.putExtra(LoginViewModel.EXTRA_AUTH_CONFIG, acc.authConfig)
+        i.putExtra(LoginViewModel.EXTRA_AUTH_STATE, acc.authState)
+        startActivity(i)
+        finish()
     }
 }

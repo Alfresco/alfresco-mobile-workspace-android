1afffd799bf4440aab4b958c8918d48d3ed3f90f
aman-alfresco <87360222+aman-alfresco@users.noreply.github.com>
2022-05-13 15:20:50 +0530

    codacy correction

--- listview/src/main/kotlin/com/alfresco/content/listview/NavigateFolderData.kt
+++ listview/src/main/kotlin/com/alfresco/content/listview/NavigateFolderData.kt
@@ -2,4 +2,7 @@ package com.alfresco.content.listview
 
 import com.alfresco.content.data.Entry
 
+/**
+ * Mark as NavigateFolderData
+ */
 data class NavigateFolderData(val isNavigate: Boolean, val entry: Entry)

91bc9a7b967e99957218fe558cc2f51d5675e3f4
aman-alfresco <87360222+aman-alfresco@users.noreply.github.com>
2022-06-23 21:16:06 +0530

    codacy correction

--- data/src/main/kotlin/com/alfresco/content/data/AnalyticsManager.kt
+++ data/src/main/kotlin/com/alfresco/content/data/AnalyticsManager.kt
@@ -1,11 +1,16 @@
 package com.alfresco.content.data
 
+/**
+ * Marked as AnalyticsManager class
+ */
 class AnalyticsManager {
 
     val repository = AnalyticsRepository()
 
+    /**
+     * analytics event for preview file
+     */
     fun previewFile(fileMimeType: String, fileExtension: String, success: Boolean) {
-        println("analytics previewFile::  $fileMimeType : $fileExtension : $success")
         val params = repository.defaultParams()
         params.putString(Parameters.FileMimeType.value, fileMimeType)
         params.putString(Parameters.FileExtension.value, fileExtension)
@@ -14,8 +19,10 @@ class AnalyticsManager {
         repository.logEvent(EventType.ActionEvent, params)
     }
 
+    /**
+     * analytics for multiple actions
+     */
     fun fileActionEvent(fileMimeType: String = "", fileExtension: String = "", eventName: EventName) {
-        println("analytics fileActionEvent::  $fileMimeType : $fileExtension ")
         val params = repository.defaultParams()
         params.putString(Parameters.FileMimeType.value, fileMimeType)
         params.putString(Parameters.FileExtension.value, fileExtension)
@@ -23,23 +30,29 @@ class AnalyticsManager {
         repository.logEvent(EventType.ActionEvent, params)
     }
 
+    /**
+     * analytics for theme change
+     */
     fun theme(name: String) {
-        println("analytics theme::  $name  ")
         val params = repository.defaultParams()
         params.putString(Parameters.ThemeName.value, name)
         params.putString(Parameters.EventName.value, EventName.ChangeTheme.value)
         repository.logEvent(EventType.ActionEvent, params)
     }
 
+    /**
+     * analytics for app launch
+     */
     fun appLaunch() {
-        println("analytics appLaunch  ")
         val params = repository.defaultParams()
         params.putString(Parameters.EventName.value, EventName.AppLaunched.value)
         repository.logEvent(EventType.ActionEvent, params)
     }
 
+    /**
+     * analytics for search facets
+     */
     fun searchFacets(name: String) {
-        println("analytics searchFacets::  $name  ")
         val params = repository.defaultParams()
         params.putString(Parameters.FacetName.value, name)
         params.putString(Parameters.EventName.value, EventName.ChangeTheme.value)

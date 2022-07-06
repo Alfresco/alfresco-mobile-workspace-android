c37f88f7d24bf02d7505cab5aecfaf746fc4c992
aman-alfresco <87360222+aman-alfresco@users.noreply.github.com>
2022-07-06 17:38:54 +0530

    added task navigation

---
+++ tasks/src/main/java/com/alfresco/tasks/TasksFragment.kt
@@ -0,0 +1,26 @@
+package com.alfresco.tasks
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+/**
+ * Marked as TaskFragment
+ */
+class TasksFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_tasks, container, false)
+    }
+}

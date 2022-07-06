c5c8ba8307625a2316f24973d1f7ba134ca54d97
aman-alfresco <87360222+aman-alfresco@users.noreply.github.com>
2022-07-06 19:19:26 +0530

    added list task fragment

--- browse/src/main/kotlin/com/alfresco/content/browse/tasks/TasksFragment.kt
+++ browse/src/main/kotlin/com/alfresco/content/browse/tasks/TasksFragment.kt
@@ -1,19 +1,16 @@
-package com.alfresco.tasks
+package com.alfresco.content.browse.tasks
 
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.alfresco.content.browse.R
 
 /**
  * Marked as TaskFragment
  */
 class TasksFragment : Fragment() {
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
---
+++ listview/src/main/kotlin/com/alfresco/content/listview/tasks/ListTaskFragment.kt
@@ -0,0 +1,197 @@
+package com.alfresco.content.listview.tasks
+
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.View
+import android.widget.FrameLayout
+import android.widget.TextView
+import androidx.core.view.isVisible
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.RecyclerView
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
+import com.airbnb.epoxy.AsyncEpoxyController
+import com.airbnb.epoxy.EpoxyRecyclerView
+import com.airbnb.mvrx.Async
+import com.airbnb.mvrx.Fail
+import com.airbnb.mvrx.Loading
+import com.airbnb.mvrx.MavericksState
+import com.airbnb.mvrx.MavericksView
+import com.airbnb.mvrx.MavericksViewModel
+import com.airbnb.mvrx.Success
+import com.airbnb.mvrx.withState
+import com.alfresco.content.actions.ContextualActionsSheet
+import com.alfresco.content.data.Entry
+import com.alfresco.content.data.ResponsePaging
+import com.alfresco.content.listview.R
+import com.alfresco.content.listview.listViewGroupHeader
+import com.alfresco.content.listview.listViewMessage
+import com.alfresco.content.listview.listViewPageBoundary
+import com.alfresco.content.listview.listViewPageLoading
+import com.alfresco.content.listview.listViewRow
+import com.alfresco.content.simpleController
+import com.alfresco.list.replace
+import com.google.android.material.button.MaterialButton
+import com.google.android.material.progressindicator.LinearProgressIndicator
+
+interface ListTaskViewState : MavericksState {
+    val entries: List<Entry>
+    val hasMoreItems: Boolean
+    val request: Async<ResponsePaging>
+    val isCompact: Boolean
+
+    fun copy(_entries: List<Entry>): ListTaskViewState
+
+    fun copyRemoving(entry: Entry) =
+        copy(entries.filter {
+            it.id != entry.id
+        })
+
+    fun copyUpdating(entry: Entry) =
+        copy(entries.replace(entry) {
+            it.id == entry.id
+        })
+}
+
+abstract class ListTaskViewModel<S : ListTaskViewState>(
+    initialState: S
+) : MavericksViewModel<S>(initialState) {
+
+    abstract fun refresh()
+    abstract fun fetchNextPage()
+    abstract fun emptyMessageArgs(state: ListTaskViewState): Triple<Int, Int, Int>
+
+    companion object {
+        const val ITEMS_PER_PAGE = 25
+        const val IS_EVENT_REGISTERED = "isEventRegistered"
+    }
+}
+
+/**
+ * Mark as ListFragment
+ */
+abstract class ListFragment<VM : ListTaskViewModel<S>, S : ListTaskViewState>(layoutID: Int = R.layout.fragment_list) :
+    Fragment(layoutID), MavericksView {
+    abstract val viewModel: VM
+
+    lateinit var loadingAnimation: View
+    lateinit var recyclerView: EpoxyRecyclerView
+    lateinit var refreshLayout: SwipeRefreshLayout
+    lateinit var loadingMessage: TextView
+    var tvUploadingFiles: TextView? = null
+    var tvPercentage: TextView? = null
+    var bannerTransferData: FrameLayout? = null
+    var uploadButton: MaterialButton? = null
+    var moveHereButton: MaterialButton? = null
+    var cancelButton: MaterialButton? = null
+    var percentageFiles: LinearProgressIndicator? = null
+    private val epoxyController: AsyncEpoxyController by lazy { epoxyController() }
+    private var delayedBoundary: Boolean = false
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        loadingAnimation = view.findViewById(R.id.loading_animation)
+        recyclerView = view.findViewById(R.id.recycler_view)
+        refreshLayout = view.findViewById(R.id.refresh_layout)
+        loadingMessage = view.findViewById(R.id.loading_message)
+        bannerTransferData = view.findViewById(R.id.banner_parent)
+
+        uploadButton = view.findViewById(R.id.upload_button)
+        moveHereButton = view.findViewById(R.id.move_here_button)
+        cancelButton = view.findViewById(R.id.cancel_button)
+        tvUploadingFiles = view.findViewById(R.id.tv_uploading_files)
+        tvPercentage = view.findViewById(R.id.tv_percentage)
+        percentageFiles = view.findViewById(R.id.percentage_files)
+
+        refreshLayout.setOnRefreshListener {
+            viewModel.refresh()
+        }
+        recyclerView.setController(epoxyController)
+
+        epoxyController.adapter.registerAdapterDataObserver(object : RecyclerView.AdapterDataObserver() {
+            override fun onItemRangeInserted(positionStart: Int, itemCount: Int) {
+                if (positionStart == 0) {
+                    // @see: https://github.com/airbnb/epoxy/issues/224
+                    recyclerView.layoutManager?.scrollToPosition(0)
+                }
+            }
+        })
+    }
+
+    override fun invalidate() = withState(viewModel) { state ->
+
+        loadingAnimation.isVisible =
+            state.request is Loading && state.entries.isEmpty() && !refreshLayout.isRefreshing
+
+        uploadButton?.isEnabled = state.request is Success
+
+        if (state.request.complete) {
+            refreshLayout.isRefreshing = false
+        }
+        epoxyController.requestModelBuild()
+    }
+
+    private fun epoxyController() = simpleController(viewModel) { state ->
+        if (state.entries.isEmpty() && state.request.complete) {
+            val args = viewModel.emptyMessageArgs(state)
+            listViewMessage {
+                id("empty_message")
+                iconRes(args.first)
+                title(args.second)
+                message(args.third)
+            }
+        } else if (state.entries.isNotEmpty()) {
+            state.entries.forEach {
+                if (it.type == Entry.Type.GROUP) {
+                    listViewGroupHeader {
+                        id(it.name)
+                        title(it.name)
+                    }
+                } else {
+                    listViewRow {
+                        id(stableId(it))
+                        data(it)
+                        compact(state.isCompact)
+                        clickListener { model, _, _, _ -> onItemClicked(model.data()) }
+                        moreClickListener { model, _, _, _ -> onItemMoreClicked(model.data()) }
+                    }
+                }
+            }
+
+            if (state.hasMoreItems) {
+                if (state.request is Loading) {
+                    listViewPageLoading {
+                        id("loading at ${state.entries.size}")
+                    }
+                } else {
+                    // On failure delay creating the boundary so that the list scrolls up
+                    // and the user has to scroll back down to activate a retry
+                    val isFail = state.request is Fail
+                    if (isFail && !delayedBoundary) {
+                        delayedBoundary = true
+                        Handler(Looper.getMainLooper()).postDelayed({ this.requestModelBuild() }, 300)
+                    } else {
+                        if (isFail) {
+                            delayedBoundary = false
+                        }
+                        listViewPageBoundary {
+                            id("boundary at ${state.entries.size}")
+                            onBind { _, _, _ -> viewModel.fetchNextPage() }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    private fun stableId(entry: Entry): String =
+        if (entry.isUpload) entry.boxId.toString()
+        else entry.id
+
+    abstract fun onItemClicked(entry: Entry)
+
+    open fun onItemMoreClicked(entry: Entry) {
+        ContextualActionsSheet.with(entry).show(childFragmentManager, null)
+    }
+}

36ea003f33b6609d6bdb1e1fa7cc4a92c243a419
aman-alfresco <87360222+aman-alfresco@users.noreply.github.com>
2022-07-06 19:55:26 +0530

    updated icons for tasks

--- browse/src/main/kotlin/com/alfresco/content/browse/tasks/TasksFragment.kt
+++ browse/src/main/kotlin/com/alfresco/content/browse/tasks/TasksFragment.kt
@@ -1,10 +1,11 @@
-package com.alfresco.tasks
+package com.alfresco.content.browse.tasks
 
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.alfresco.content.browse.R
 
 /**
  * Marked as TaskFragment

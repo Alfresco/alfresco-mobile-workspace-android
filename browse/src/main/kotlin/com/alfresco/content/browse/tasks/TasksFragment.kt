efebbe465a10deab69c5eb31c86bccd61b5bf99b
Amanpal Singh <87360222+aman-alfresco@users.noreply.github.com>
2022-07-15 08:31:52 +0530

    Mobileapps 1270 (#136)
    * added list task fragment
    
    * updated icons for tasks
    
    * removed unused code
    
    * added background xml
    
    * load next page
    
    * added pagination for task list
    
    * added localisation
    
    * linked process api
    
    * removed unused code
    
    * codacy correction

--- browse/src/main/kotlin/com/alfresco/content/browse/tasks/TasksFragment.kt
+++ browse/src/main/kotlin/com/alfresco/content/browse/tasks/TasksFragment.kt
@@ -1,27 +1,22 @@
 package com.alfresco.content.browse.tasks
 
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
-import androidx.fragment.app.Fragment
-import com.alfresco.content.browse.R
+import com.airbnb.mvrx.fragmentViewModel
+import com.alfresco.content.listview.tasks.TaskListFragment
 
 /**
- * Marked as TaskFragment
+ * Marked as TasksFragment
  */
-class TasksFragment : Fragment() {
+class TasksFragment : TaskListFragment<TasksViewModel, TasksViewState>() {
+
+    override val viewModel: TasksViewModel by fragmentViewModel()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_tasks, container, false)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
     }
 }
---
+++ browse/src/main/kotlin/com/alfresco/content/browse/tasks/TasksViewState.kt
@@ -0,0 +1,55 @@
+package com.alfresco.content.browse.tasks
+
+import com.airbnb.mvrx.Async
+import com.airbnb.mvrx.Uninitialized
+import com.alfresco.content.data.Entry
+import com.alfresco.content.data.ResponseList
+import com.alfresco.content.data.TaskEntry
+import com.alfresco.content.data.Tasks
+import com.alfresco.content.data.Tasks.Active
+import com.alfresco.content.listview.tasks.TaskListViewState
+
+/**
+ * Marked as TasksViewState class
+ */
+data class TasksViewState(
+    val parent: Entry? = null,
+    override val taskEntries: List<TaskEntry> = emptyList(),
+    override val hasMoreItems: Boolean = false,
+    override val request: Async<ResponseList> = Uninitialized,
+    val displayTask: Tasks = Active,
+    val loadItemsCount: Int = 0,
+    val page: Int = 0
+) : TaskListViewState {
+
+    override val isCompact = false
+
+    override fun copy(_entries: List<TaskEntry>) = copy(taskEntries = _entries)
+
+    /**
+     * update the latest response
+     */
+    fun update(
+        response: ResponseList?
+    ): TasksViewState {
+        if (response == null) return this
+
+        var totalLoadCount = 0
+
+        val taskPageEntries = response.listTask
+
+        val newTaskEntries = if (response.start != 0) {
+            totalLoadCount = loadItemsCount.plus(response.size)
+            taskEntries + taskPageEntries
+        } else {
+            totalLoadCount = response.size
+            taskPageEntries
+        }
+
+        return copy(
+            taskEntries = newTaskEntries,
+            loadItemsCount = totalLoadCount,
+            hasMoreItems = totalLoadCount < response.total
+        )
+    }
+}

99c85ef0720dbdc4fd6d5e2db4b490142712b41b
Amanpal Singh <87360222+aman-alfresco@users.noreply.github.com>
2022-10-17 11:07:15 +0530

    codacy correction

--- browse/src/main/kotlin/com/alfresco/content/browse/tasks/detail/TaskDetailViewState.kt
+++ browse/src/main/kotlin/com/alfresco/content/browse/tasks/detail/TaskDetailViewState.kt
@@ -104,6 +104,9 @@ data class TaskDetailViewState(
             }
         }
 
+    /**
+     * removed the delete entry from existing content list.
+     */
     fun updateDelete(contentId: String): TaskDetailViewState {
         val filterList = listContents.filter { it.id != contentId }
         return copy(listContents = filterList)

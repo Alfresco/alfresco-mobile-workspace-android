2c6c9919f5c85c8f2b00756e3e2cb9a133675cac
Amanpal Singh <87360222+aman-alfresco@users.noreply.github.com>
2022-11-23 10:44:46 +0530

    added accessibility text on settings page

--- app/src/main/java/com/alfresco/content/app/widget/AccountPreference.kt
+++ app/src/main/java/com/alfresco/content/app/widget/AccountPreference.kt
@@ -3,6 +3,7 @@ package com.alfresco.content.app.widget
 import android.content.Context
 import android.util.AttributeSet
 import android.view.View
+import android.widget.LinearLayout
 import androidx.preference.Preference
 import androidx.preference.PreferenceViewHolder
 import com.alfresco.content.app.R
@@ -11,11 +12,15 @@ import com.google.android.material.button.MaterialButton
 class AccountPreference(context: Context, attrs: AttributeSet?) : Preference(context, attrs) {
 
     private lateinit var signOutButton: MaterialButton
+    private lateinit var parentUserInfo: LinearLayout
     var onSignOutClickListener: View.OnClickListener? = null
+    var accessibilityTextUserInfo = ""
 
     override fun onBindViewHolder(holder: PreferenceViewHolder) {
         super.onBindViewHolder(holder)
         signOutButton = holder.findViewById(R.id.sign_out_button) as MaterialButton
+        parentUserInfo = holder.findViewById(R.id.parent_user_info) as LinearLayout
+        parentUserInfo.contentDescription = accessibilityTextUserInfo
         signOutButton.setOnClickListener(onSignOutClickListener)
     }
 }

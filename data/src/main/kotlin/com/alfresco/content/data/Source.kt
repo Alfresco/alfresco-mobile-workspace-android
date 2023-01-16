9ff57175f76c4e2956496065c045b95800d17a2f
Amanpal Singh <87360222+aman-alfresco@users.noreply.github.com>
2023-01-16 10:35:32 +0530

    codacy correction

--- data/src/main/kotlin/com/alfresco/content/data/Source.kt
+++ data/src/main/kotlin/com/alfresco/content/data/Source.kt
@@ -2,7 +2,14 @@ package com.alfresco.content.data
 
 import com.alfresco.content.models.Node
 
+/**
+ * Mark as Source class
+ */
 data class Source(val name: String) {
+
+    /**
+     * returns obj as Source type.
+     */
     companion object {
         fun with(node: Node?): Source {
             return Source(name = node?.name ?: "")

343c323cdc2170cb158379310ea1e2f7125b9dbf
Amanpal Singh <87360222+aman-alfresco@users.noreply.github.com>
2023-01-30 10:47:23 +0530

    fixed title on personal files (#213)

--- data/src/main/kotlin/com/alfresco/content/data/Source.kt
+++ data/src/main/kotlin/com/alfresco/content/data/Source.kt
@@ -5,13 +5,15 @@ import com.alfresco.content.models.Node
 /**
  * Mark as Source class
  */
-data class Source(val name: String) {
+data class Source(val name: String? = null) {
     companion object {
         /**
          * returns obj as Source type.
          */
-        fun with(node: Node?): Source {
-            return Source(name = node?.name ?: "")
+        fun with(node: Node?, email: String?): Source {
+            return if (!email.equals(node?.name))
+                Source(name = node?.name)
+            else Source()
         }
     }
 }

f26e6c02276015f09a5982ee80e2a5a47e4cb7f7
Amanpal Singh <87360222+aman-alfresco@users.noreply.github.com>
2023-05-24 15:26:28 +0530

    MOBILEAPPS-1870 (#245)
    * added account info api to extract source
    
    * removed unnecessary libs
    
    * codacy correction
    
    * updated file in queue message

---
+++ data/src/main/kotlin/com/alfresco/content/data/AccountInfoData.kt
@@ -0,0 +1,46 @@
+package com.alfresco.content.data
+
+import com.alfresco.process.models.AccountInfo
+import java.time.ZonedDateTime
+
+/**
+ * Marked as AccountInfoData
+ */
+data class AccountInfoData(
+    val id: Int? = null,
+    val tenantId: Int? = null,
+    val created: ZonedDateTime? = null,
+    val lastUpdated: ZonedDateTime? = null,
+    val alfrescoTenantId: String? = null,
+    val repositoryUrl: String? = null,
+    val shareUrl: String? = null,
+    val name: String? = null,
+    val secret: String? = null,
+    val authenticationType: String? = null,
+    val version: String? = null,
+    val sitesFolder: String? = null
+) {
+    val sourceName = if (id != null && !name.isNullOrEmpty()) "alfresco-$id-${name}Alfresco" else "undefinedAlfresco"
+
+    companion object {
+        /**
+         * returns the AccountInfoData obj by using AccountInfo obj
+         */
+        fun with(raw: AccountInfo): AccountInfoData {
+            return AccountInfoData(
+                id = raw.id,
+                tenantId = raw.tenantId,
+                created = raw.created,
+                lastUpdated = raw.lastUpdated,
+                alfrescoTenantId = raw.alfrescoTenantId,
+                repositoryUrl = raw.repositoryUrl,
+                shareUrl = raw.shareUrl,
+                name = raw.name,
+                secret = raw.secret,
+                authenticationType = raw.authenticationType,
+                version = raw.version,
+                sitesFolder = raw.sitesFolder
+            )
+        }
+    }
+}
---
+++ data/src/main/kotlin/com/alfresco/content/data/ResponseAccountInfo.kt
@@ -0,0 +1,27 @@
+package com.alfresco.content.data
+
+import com.alfresco.process.models.ResultAccountInfo
+
+/**
+ * Marked as ResponseAccountInfo
+ */
+data class ResponseAccountInfo(
+    val size: Int? = null,
+    val start: Int? = null,
+    val total: Int? = null,
+    val listAccounts: List<AccountInfoData> = emptyList()
+) {
+    companion object {
+        /**
+         * returns the ResponseAccountInfo obj by using ResultAccountInfo obj
+         */
+        fun with(raw: ResultAccountInfo): ResponseAccountInfo {
+            return ResponseAccountInfo(
+                size = raw.size,
+                start = raw.start,
+                total = raw.total,
+                listAccounts = raw.listAccountInfo?.map { AccountInfoData.with(it) } ?: listOf(AccountInfoData())
+            )
+        }
+    }
+}

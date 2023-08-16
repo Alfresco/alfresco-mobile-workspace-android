f763e90ae7b728debfc98b3ffb2c611a7c5f83fe
Amanpal Singh <87360222+aman-alfresco@users.noreply.github.com>
2023-08-16 10:10:07 +0530

    added offline actions

--- actions/src/main/kotlin/com/alfresco/content/actions/ContextualActionsViewModel.kt
+++ actions/src/main/kotlin/com/alfresco/content/actions/ContextualActionsViewModel.kt
@@ -130,16 +130,18 @@ class ContextualActionsViewModel(
 
         when {
             state.entries.all { it.isTrashed } -> {
+                // Added restore and delete actions
                 actions.add(ActionRestore(entry, state.entries))
                 actions.add(ActionDeleteForever(entry, state.entries))
             }
 
             state.entries.all { it.hasOfflineStatus } -> {
-                // Added Offline Action
+                // Added Offline action
                 actions.add(offlineMultiActionFor(entry, state.entries))
             }
 
             else -> {
+                // Added common actions
                 actions.addAll(sharedActions(entry, state.entries))
             }
         }

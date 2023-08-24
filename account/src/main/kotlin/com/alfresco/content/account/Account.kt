59b93545e2f6a8e57e32a4eed3af6bcf74688dc1
Amanpal Singh <87360222+aman-alfresco@users.noreply.github.com>
2023-08-24 12:36:27 +0530

    added creds in secure shared prefs

--- account/src/main/kotlin/com/alfresco/content/account/Account.kt
+++ account/src/main/kotlin/com/alfresco/content/account/Account.kt
@@ -3,6 +3,9 @@ package com.alfresco.content.account
 import android.accounts.AccountManager
 import android.content.Context
 import android.os.Bundle
+import com.alfresco.content.account.SecureSharedPreferencesManager.Companion.KEY_DISPLAY_NAME
+import com.alfresco.content.account.SecureSharedPreferencesManager.Companion.KEY_EMAIL
+import com.alfresco.content.account.SecureSharedPreferencesManager.Companion.KEY_PASSWORD
 import android.accounts.Account as AndroidAccount
 
 data class Account(
@@ -34,22 +37,31 @@ data class Account(
             email: String,
             myFiles: String,
         ) {
+            val sharedSecure = SecureSharedPreferencesManager(context)
+
             val b = Bundle()
             b.putString(authTypeKey, authType)
             b.putString(authConfigKey, authConfig)
             b.putString(serverKey, serverUrl)
-            b.putString(displayNameKey, displayName)
-            b.putString(emailKey, email)
+            b.putString(displayNameKey, KEY_DISPLAY_NAME)
+            b.putString(emailKey, KEY_EMAIL)
             b.putString(myFilesKey, myFiles)
             val acc = AndroidAccount(id, context.getString(R.string.android_auth_account_type))
-            AccountManager.get(context).addAccountExplicitly(acc, authState, b)
+
+            // Save credentials securely using the SecureSharedPreferencesManager
+            sharedSecure.saveCredentials(email, authState, displayName)
+
+            AccountManager.get(context).addAccountExplicitly(acc, KEY_PASSWORD, b)
         }
 
         fun update(context: Context, id: String, authState: String) {
             val am = AccountManager.get(context)
             val acc = getAndroidAccount(context)
+            val sharedSecure = SecureSharedPreferencesManager(context)
             if (acc?.name == id) {
-                am.setPassword(acc, authState)
+                // Save credentials securely using the SecureSharedPreferencesManager
+                sharedSecure.savePassword(authState)
+                am.setPassword(acc, KEY_PASSWORD)
             }
         }
 
@@ -61,11 +73,16 @@ data class Account(
             email: String,
             myFiles: String,
         ) {
+            val sharedSecure = SecureSharedPreferencesManager(context)
             val am = AccountManager.get(context)
             val acc = getAndroidAccount(context)
-            am.setPassword(acc, authState)
-            am.setUserData(acc, displayNameKey, displayName)
-            am.setUserData(acc, emailKey, email)
+
+            // Save credentials securely using the SecureSharedPreferencesManager
+            sharedSecure.saveCredentials(email, authState, displayName)
+
+            am.setPassword(acc, KEY_PASSWORD)
+            am.setUserData(acc, displayNameKey, KEY_DISPLAY_NAME)
+            am.setUserData(acc, emailKey, KEY_EMAIL)
             am.setUserData(acc, myFilesKey, myFiles)
 
             if (acc?.name != id) {
@@ -83,16 +100,19 @@ data class Account(
         fun getAccount(context: Context): Account? {
             val am = AccountManager.get(context)
             val accountList = am.getAccountsByType(context.getString(R.string.android_auth_account_type))
-            if (accountList.isNotEmpty()) {
+            val sharedSecure = SecureSharedPreferencesManager(context)
+            // get credentials securely using the SecureSharedPreferencesManager
+            val secureCredentials = sharedSecure.getSavedCredentials()
+            if (accountList.isNotEmpty() && secureCredentials != null) {
                 val acc = accountList[0]
                 return Account(
                     acc.name,
-                    am.getPassword(acc),
+                    secureCredentials.second,
                     am.getUserData(acc, authTypeKey),
                     am.getUserData(acc, authConfigKey),
                     am.getUserData(acc, serverKey),
-                    am.getUserData(acc, displayNameKey),
-                    am.getUserData(acc, emailKey),
+                    secureCredentials.third,
+                    secureCredentials.first,
                     am.getUserData(acc, myFilesKey),
                 )
             }

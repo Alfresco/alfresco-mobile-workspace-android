3a4d1b0b10c4f1f947eeaa95d97cb7415f6b5c3a
Amanpal Singh <87360222+aman-alfresco@users.noreply.github.com>
2025-12-17 11:19:19 +0530

    updated dark theme icons

--- app/src/main/java/com/alfresco/content/app/widget/AskDiscoveryIconView.kt
+++ app/src/main/java/com/alfresco/content/app/widget/AskDiscoveryIconView.kt
@@ -4,10 +4,12 @@ import android.content.Context
 import android.net.Uri
 import android.util.AttributeSet
 import android.view.LayoutInflater
+import android.widget.ImageView
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.core.content.ContextCompat
 import androidx.core.view.isVisible
 import coil.load
+import coil.size.Scale
 import coil.transform.CircleCropTransformation
 import com.alfresco.content.app.R
 import com.alfresco.content.app.databinding.ViewAskDiscoveryIconBinding
@@ -34,11 +36,4 @@ class AskDiscoveryIconView(
 
     constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int) :
         this(context, attrs, defStyleAttr, 0)
-
-    fun loadIcon(uri: Uri) {
-        binding.icon.load(uri) {
-            placeholder(R.drawable.ask_discovery_icon)
-            error(R.drawable.ask_discovery_icon)
-        }
-    }
 }
---
+++ base-ui/src/main/kotlin/com/alfresco/ui/MaterialShapeADView.kt
@@ -0,0 +1,109 @@
+package com.alfresco.ui
+
+import android.content.Context
+import android.content.res.ColorStateList
+import android.content.res.TypedArray
+import android.graphics.drawable.ColorDrawable
+import android.util.AttributeSet
+import android.widget.FrameLayout
+import androidx.annotation.Dimension
+import androidx.core.view.ViewCompat
+import com.google.android.material.shape.MaterialShapeDrawable
+import com.google.android.material.shape.ShapeAppearanceModel
+
+class MaterialShapeADView(
+    context: Context,
+    attrs: AttributeSet?,
+    defStyleAttr: Int,
+    defStyleRes: Int,
+) : FrameLayout(context, attrs, defStyleAttr, defStyleRes) {
+    private val background = MaterialShapeDrawable(context, attrs, defStyleAttr, defStyleRes)
+    private var onElevation: ((Float) -> Unit)? = null
+
+    var backgroundColor: ColorStateList? = background.fillColor
+        set(value) {
+            field = value
+            background.fillColor = value
+        }
+
+    var shapeAppearanceModel: ShapeAppearanceModel = background.shapeAppearanceModel
+        set(value) {
+            field = value
+            background.shapeAppearanceModel = value
+        }
+
+    var radius: Float
+        get() = background.topLeftCornerResolvedSize
+        set(value) {
+            shapeAppearanceModel = shapeAppearanceModel.withCornerSize(value)
+        }
+
+    @Dimension
+    var strokeWidth: Int = 0
+        set(value) {
+            field = value
+            updateStroke()
+        }
+
+    var strokeColor: ColorStateList? = null
+        set(value) {
+            field = value
+            updateStroke()
+        }
+
+    override fun setElevation(elevation: Float) {
+        super.setElevation(elevation)
+        onElevation?.invoke(elevation)
+    }
+
+    init {
+        ViewCompat.setBackground(this, background)
+
+        // Load attributes
+        val attributes =
+            context.obtainStyledAttributes(
+                attrs,
+                R.styleable.MaterialShapeView,
+                defStyleAttr,
+                defStyleRes,
+            )
+
+        try {
+            loadFromAttributes(attributes)
+        } finally {
+            attributes.recycle()
+        }
+
+        radius = 32.0f
+
+        foreground = ColorDrawable()
+
+        // Deferred elevation as this is called during base class init and would otherwise crash
+        onElevation = { elevation ->
+            background.elevation = elevation
+        }
+        background.initializeElevationOverlay(context)
+        onElevation?.invoke(elevation)
+    }
+
+    constructor(context: Context) :
+        this(context, null)
+
+    constructor(context: Context, attrs: AttributeSet?) :
+        this(context, attrs, 0)
+
+    constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int) :
+        this(context, attrs, defStyleAttr, 0)
+
+    private fun loadFromAttributes(attrs: TypedArray) {
+        strokeColor = attrs.getColorStateList(R.styleable.MaterialShapeView_strokeColor)
+        strokeWidth = attrs.getDimensionPixelSize(R.styleable.MaterialShapeView_strokeWidth, 0)
+        val backgroundColor = attrs.getColorStateList(R.styleable.MaterialShapeView_backgroundColor)
+        background.fillColor = backgroundColor
+        updateStroke()
+    }
+
+    private fun updateStroke() {
+        background.setStroke(strokeWidth.toFloat(), strokeColor)
+    }
+}
